7560cec42d71980e7bfc55f21613f0b2670bd1f9
Dmitry Dobrynin <dobrynya@inbox.ru>
2013-08-20 23:07:25 +0400

    Got rid of ChatManager.

--- app/controllers/Chat.scala
+++ app/controllers/Chat.scala
@@ -13,25 +13,15 @@ case class User(name: String, channel: Concurrent.Channel[JsValue]) {
   }
 }
 
-case class Message(recipient: String, text: String)
-
 object Chat extends Controller {
+  var loggedInUsers = Map.empty[String, User]
+
   def index = Action {implicit request =>
     Ok(views.html.chat())
   }
 
-  def logIn(name: String) = WebSocket.using[JsValue] {implicit request =>
-    println("received request to log in from user %s!" format name)
-    ChatManager.logIn(name)
-  }
-}
-
-object ChatManager {
-  var loggedInUsers = Map.empty[String, User]
-
-  def users = loggedInUsers.toList.map(_._2).sortBy(_.name)
-
-  def logIn(username: String) = {
+  def logIn(username: String) = WebSocket.using[JsValue] {implicit request =>
+    println("received request to log in from user %s!" format username)
     if (loggedInUsers.contains(username))
       (Iteratee.ignore[JsValue],
         Enumerator[JsValue](Json.obj("kind" -> "error",
@@ -42,16 +32,17 @@ object ChatManager {
 
       val iteratee = Iteratee.foreach[JsValue] {m =>
         println("Received request: " + m)
-        processMessage(Message((m \ "recipient").as[String], (m \ "text").as[String]), user)
+        processMessage((m \ "recipient").as[String], (m \ "text").as[String], user)
       } mapDone {_ =>
         println("User %s is disconnected!" format username)
-        ChatManager.disconnected(user)
+        disconnected(user)
       }
 
       connected(user)
 
-      (iteratee, Enumerator[JsValue](Json.obj("kind" -> "connected", "user" -> user.name,
-          "users" -> loggedInUsers.toList.map(_._2.name))) >>> enumerator)
+      (iteratee,
+        Enumerator[JsValue](Json.obj("kind" -> "connected", "user" -> user.name,
+        "users" -> loggedInUsers.toList.map(_._2.name))) >>> enumerator)
     }
   }
 
@@ -69,8 +60,8 @@ object ChatManager {
     broadcast(Json.obj("kind" -> "connected", "user" -> user.name, "users" -> loggedInUsers.toList.map(_._2.name)))
   }
 
-  def processMessage(message: Message, sender: User) {
-    println("Processing message %s from user %s".format(message, sender.name))
-    loggedInUsers(message.recipient).send(Json.obj("kind" -> "message", "sender" -> sender.name, "text" -> message.text))
+  def processMessage(recipient: String, text: String, sender: User) {
+    println("Processing message from %s to %s".format(sender.name, recipient))
+    loggedInUsers(recipient).send(Json.obj("kind" -> "message", "sender" -> sender.name, "text" -> text))
   }
 }
